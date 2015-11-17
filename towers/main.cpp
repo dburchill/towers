05553d093f0fd2821c6fb03c1f0d933f6665fff4
Dave Burchill <dave@burchill.ca>
2015-11-16 22:03:47 -0400

    Initial Commit

---
+++ towers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  towers
+//
+//  Created by Dave Burchill on 2015-11-16.
+//  Copyright © 2015 burchill.ca. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
